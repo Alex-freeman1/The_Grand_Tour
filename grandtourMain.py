52a235833904c96fd3a48ca351424c0185dd7c34
Alexy <alexander.j.freeman@gmail.com>
2025-05-15 12:06:46 +1200

    updated code for push

--- grandtourMain.py
+++ grandtourMain.py
@@ -18,6 +18,10 @@ from datetime import datetime, timedelta
 import lambertSolver as lt
 import planetary_data as pd
 
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+
 # plt.style.use( 'dark_background' )
 
 
@@ -25,39 +29,20 @@ import planetary_data as pd
 '''
 Setting up Ephemeris Data and Kernels
 '''
-
-
 # Establish Kernels
 spice.furnsh("de432s\de432s.bsp")
 spice.furnsh("data\latest_leapseconds (1).tls")
 
-
-# Input code - this is variable, please change to whatever you want
-
-
-# departure0 = '2028-09-15'         
-# departure1 = '2029-06-01'  
-# arrival0 = '2029-05-01'         
-# arrival1 = '2030-04-01'
 
 # Step size (in days)
 step_size = 20
 
 # --------------------
 
-
-
-
-
 # Frame of reference from the planetary data
 OBSERVER= pd.sun['name']
 FRAME='ECLIPJ2000'
 sun_mu = pd.sun['mu']
-
-
-
-
-
 
 
 
@@ -70,11 +55,7 @@ step = step_size*3600*24
 
 
 
-'''
-Using Lambert's Solver to return the required velocities
-'''
-
-          
+         
 
 #Define function to get the emphemeris data - array holding positions and velocity vectors
 def calc_ephemeris(target, ets, frame, observer):
@@ -84,111 +65,71 @@ def calc_ephemeris(target, ets, frame, observer):
 def norm(vec):
     return np.linalg.norm(vec)
 
-def johann(dep_planet, arr_planet, dep_dates_i, dep_dates_f, arr_dates_i, arr_dates_f):
+
+
+def johann(dep_planet, arr_planet, departure0, departure1, arrival0, arrival1):
     
-    et_departures = np.arange(spice.utc2et(dep_dates_i), spice.utc2et(dep_dates_f) + step, step)
-    et_arrivals   = np.arange(spice.utc2et(arr_dates_i), spice.utc2et(arr_dates_f) + step, step)
-               
-   
+    et_departures = np.arange(spice.utc2et(departure0), spice.utc2et(departure1) + step, step)
+    et_arrivals   = np.arange(spice.utc2et(arrival0), spice.utc2et(arrival1) + step, step)
+    
     
     ds = len(et_departures)
     as_ = len(et_arrivals)
-    
-    x = np.arange(ds)
-    y = np.arange(as_) 
-    
     C3_shorts     = np.zeros( (as_, ds) )
     C3_longs      = np.zeros( (as_, ds) )
-    #tofs          = np.zeros( (as_, ds) )  
+    #tofs          = np.zeros( (as_, ds) )
+    total = as_ * ds   
+    
+    # Retrieve the position and velocity vectors and store in a sixth-lengthed vector from function above
+    ephem_departures = [calc_ephemeris(departure_planet, et, FRAME, OBSERVER) for et in et_departures]
+    ephem_arrivals = [calc_ephemeris(arrival_planet, et, FRAME, OBSERVER) for et in et_arrivals]
+    
+    
+    
+    
+    def compute_lambert_entry(na, nd):
+        
+        states_depart = ephem_departures[nd]
+        states_arrive = ephem_arrivals[na]
+        tof = et_arrivals[na] - et_departures[nd]
+
+        if tof <= 0 or norm(states_depart[:3] - states_arrive[:3]) < 1e6:
+            return cutoff_c3, cutoff_c3, tof
+
+        try:
+            v_sc_depart_short, _ = lt.lambert_solver(states_depart[:3], states_arrive[:3], tof, sun_mu, trajectory='pro')
+        except:
+            v_sc_depart_short = states_depart[3:] + 1000
+
+        try:
+            v_sc_depart_long, _ = lt.lambert_solver(states_depart[:3], states_arrive[:3], tof, sun_mu, trajectory='retro')
+        except:
+            v_sc_depart_long = states_depart[3:] + 1000
+
+        C3_short = min(norm(v_sc_depart_short - states_depart[3:])**2, cutoff_c3)
+        C3_long = min(norm(v_sc_depart_long - states_depart[3:])**2, cutoff_c3)
+        
+        return C3_short, C3_long, tof
+
+
+    results = Parallel(n_jobs=-1)(
+        delayed(compute_lambert_entry)(na, nd)
+        for na in tqdm(range(as_), desc="Calculating Transfer")
+        for nd in range(ds))
+
+    for idx, (C3_short, C3_long, tof) in enumerate(results):
+        na = idx // ds
+        nd = idx % ds
+        C3_shorts[na, nd] = C3_short
+        C3_longs[na, nd] = C3_long
+        #tofs[na, nd] = tof
+    
+
+               
     
    
+    
 
-    total = as_ * ds   
-           
-    # For each combination of departure and arrivals
-    for na in y:
-        for nd in x:
-        
-            
-            # Retrieve the position and velocity vectors and store in a sixth-lengthed vector from function above
-            states_depart = calc_ephemeris(departure_planet, et_departures[nd], FRAME, OBSERVER)
-            states_arrive = calc_ephemeris(arrival_planet, et_arrivals[na], FRAME, OBSERVER)
-            
-            
-            
-            # The time of flight for each orbital transfer
-            tof = (et_arrivals[na] - et_departures[nd])
-    
-            # Attempt to solve Lambert's problem for velocities
-    
-            # Short way (prograde)
-            try:
-                v_sc_depart_short, v_sc_arrive_short = lt.lambert_solver(
-                    # states_arrive is a 6 element vector, the first three represent the position vector: x,y,z
-                    # the final three represent the velocity vector: x,y,z.
-                    # therefore :3 is taking the first three position vectors passing it as R1
-                    states_depart[ :3 ],
-                    states_arrive[ :3 ], #R2
-                    tof,
-                    sun_mu,
-                    trajectory='pro'
-                )
-            except:
-                v_sc_depart_short = np.array( [1000, 1000, 1000] )
-                v_sc_arrive_short = np.array( [1000, 1000, 1000] )
-                
-                
-            
-            
-            # Long way (retrograde)
-            try:
-                v_sc_depart_long, v_sc_arrive_long = lt.lambert_solver(
-                    states_depart[ :3 ],
-                    states_arrive[ :3 ],
-                    tof,
-                    sun_mu,
-                    trajectory='retro'
-                )
-            except:
-               
-                v_sc_depart_long = np.array( [1000, 1000, 1000] )
-                v_sc_arrive_long = np.array( [1000, 1000, 1000] )
-                
-                
-            
-            
-            # Calculate C3 values at departure
-            
-            # states_arrive is a 6 element vector, the first three represent the position vector: x,y,z
-            # the final three represent the velocity vector: x,y,z.
-            C3_short = norm(v_sc_depart_short - states_depart[ 3: ]) ** 2
-            C3_long  = norm( v_sc_depart_long  - states_depart[ 3: ] ) ** 2
-    
-            # Check for unreasonable values (C3)
-            if C3_short > cutoff_c3: 
-                C3_short = cutoff_c3
-            if C3_long  > cutoff_c3: 
-                C3_long = cutoff_c3
-    
-            # Calculate v_infinity values at arrival
-            v_inf_short = norm( v_sc_arrive_short - states_arrive[ 3: ] ) 
-            v_inf_long  = norm( v_sc_arrive_long  - states_arrive[ 3: ] )
-    
-            # Check for unreasonable values (v_infinity)
-            if v_inf_short > cutoff_v: 
-                v_inf_short = cutoff_v
-            if v_inf_long  > cutoff_v: 
-                v_inf_long  = cutoff_v
-    
-            # Append values to corresponding arrays
-            C3_shorts    [ na, nd ] = C3_short
-            C3_longs     [ na, nd ] = C3_long
-            # v_inf_shorts [ na, nd ] = v_inf_short
-            # v_inf_longs  [ na, nd ] = v_inf_long
-            #tofs         [ na, nd ] = tof
-           
-    
-        print( f'{na + 1} / {as_}.' )
     
     
     # Prints the combination numver its calculating
@@ -225,10 +166,7 @@ def loop_bodies(planeti, planetf):
         
     
         
-    
-    
-    
-    
+ 
 
 '''
 
@@ -276,10 +214,12 @@ minium value finder
 planet0 = 'Earth' #Case sensitive
 planet1 = 'Mars'
 # Dates for Launchwindow - wikipedia
-dep_dates_0 = '2026-01-01'         # Intial departure date
-dep_dates_1 = '2030-01-01'         # Final departure date
-arr_dates_0 = '2026-06-01'         # Initial arrival date
-arr_dates_1 = '2030-06-01'          # Final arrival date 
+
+
+dep_dates_0 = '2005-06-20'         # Intial departure date
+dep_dates_1 = '2010-11-07'         # Final departure date
+arr_dates_0 = '2005-12-01'         # Initial arrival date
+arr_dates_1 = '2015-02-24'          # Final arrival date 
 # Holds the spice name of the planets  
 departure_planet, arrival_planet = loop_bodies(planet0, planet1)
 johann_1 = johann(departure_planet, arrival_planet, dep_dates_0, dep_dates_1, arr_dates_0, arr_dates_1)   
@@ -289,10 +229,10 @@ johann_1 = johann(departure_planet, arrival_planet, dep_dates_0, dep_dates_1, ar
 planet0 = 'Mars' #Case sensitive
 planet1 = 'Jupiter'
 departure_planet, arrival_planet = loop_bodies(planet0, planet1)
-dep_dates_1_i = '2028-02-01'       
-dep_dates_1_f = '2032-12-01'  
-arr_dates_1_i = '2029-08-01'         
-arr_dates_1_f = '2033-01-01'
+dep_dates_1_i = '2006-02-01'       
+dep_dates_1_f = '2020-12-01'  
+arr_dates_1_i = '2007-08-01'         
+arr_dates_1_f = '2025-01-01'
 johann_2 = johann(departure_planet, arrival_planet, dep_dates_1_i, dep_dates_1_f, arr_dates_1_i, arr_dates_1_f)   
 
 
@@ -300,10 +240,10 @@ johann_2 = johann(departure_planet, arrival_planet, dep_dates_1_i, dep_dates_1_f
 planet0 = 'Jupiter' #Case sensitive
 planet1 = 'Saturn'
 departure_planet, arrival_planet = loop_bodies(planet0, planet1)
-dep_dates_2_i =  "2032-01-01"
-dep_dates_2_f = "2042-01-01"
-arr_dates_2_i = "2037-01-01"
-arr_dates_2_f = "2038-01-01"
+dep_dates_2_i =  "2009-01-01"
+dep_dates_2_f = "2030-01-01"
+arr_dates_2_i = "2011-01-01"
+arr_dates_2_f = "2040-01-01"
 johann_3 = johann(departure_planet, arrival_planet, dep_dates_2_i, dep_dates_2_f, arr_dates_2_i, arr_dates_2_f)   
 
 
@@ -320,13 +260,9 @@ plot snake step
 '''
 
 
-
-
-
-
 # Define linewdith
 lw = 1.5
-fig = plt.figure(figsize=(12,12))
+fig = plt.figure(figsize=(20,20))
 
 n_plots = 3  # Adjust this as needed
 
@@ -334,10 +270,13 @@ w, h = 0.3, 0.3  # Size of each subplot
 x, y = 0.1, 0.1  # Initial position
 
 axes = []
-c3_levels_low = np.arange( 10, 50, 3)
-c3_levels_high = np.arange( 10, 500, 10)
 
-energy_levels = [c3_levels_low, c3_levels_high]
+c3_levels_0 = np.arange(10, 55, 10)
+c3_levels_1 = np.arange( 10, 500, 50)
+c3_levels_2 = np.arange( 10, 500, 100)
+
+energy_levels = [c3_levels_0, c3_levels_1, c3_levels_2]
+
 
 segment_names = ["Earth to Mars", "Mars to Jupiter", "Jupiter to Saturn"]
 date_names = (
@@ -348,14 +287,19 @@ date_names = (
 for i in range(n_plots):
     
     ax = fig.add_axes([x, y, w, h])
-    
+    plt.legend(
+      [
+          'C3 ($\dfrac{km^2}{s^2}$)',
+         
+      ],
+      bbox_to_anchor = ( 1.005, 1.01 ),
+      fontsize = 10
+      )
     # Even plots: original orientation
     if i % 2 == 0:
-        if i == 0:
-            cont  = ax.contour(date_axis[i][0], date_axis[i][1], c3_shorts_arrays[i], levels=energy_levels[0], colors='m', linewidths = lw)
-        elif i == 2:
-            cont  = ax.contour(date_axis[i][0], date_axis[i][1], c3_shorts_arrays[i], levels=energy_levels[1], colors='m', linewidths = lw)
         
+        cont  = ax.contour(date_axis[i][0], date_axis[i][1], c3_shorts_arrays[i], levels=energy_levels[i], colors='m', linewidths = lw)
+        plt.clabel( cont, fmt = '%i')
         ax.set_xlabel(f"Departure (Days Past {date_names[i][0]})")
         ax.set_ylabel(f"Arrival (Days Past {date_names[i][2]})")
         ax.xaxis.set_ticks_position('top')
@@ -371,7 +315,9 @@ for i in range(n_plots):
         # ref_ax = axes[i-1]
         # matching_yticks = ref_ax.get_yticks()
         Z_rot = np.rot90(c3_shorts_arrays[i])
-        cont  = ax.contour(date_axis[i][1], date_axis[i][0][::-1],  Z_rot, levels=energy_levels[0],colors='m', linewidths = lw)
+        cont  = ax.contour(date_axis[i][1], date_axis[i][0][::-1],  Z_rot, levels=energy_levels[i],colors='m', linewidths = lw)
+        plt.clabel( cont, fmt = '%i')
+        
         ax.set_ylabel(f"Departure (Days Past {date_names[i][0]})")
         ax.set_xlabel(f"Arrival (Days Past {date_names[i][2]})")
         ax.xaxis.set_ticks_position('bottom')
